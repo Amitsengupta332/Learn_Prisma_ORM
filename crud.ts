41a283698116b76bb01444099181ebb9fdf6efa1
amitsengupta <amitsengupta332@gmail.com>
2025-12-25 14:21:28 +0600

    Creating data with Prisma

---
+++ crud.ts
@@ -0,0 +1,41 @@
+import { prisma } from "./lib/prisma";
+
+async function run() {
+  // const createUser = await prisma.user.create({
+  //     data: {
+  //         name: "jose",
+  //         email: "jose@prisma",
+  //     }
+  // })
+
+  // console.log(createUser)
+
+  // create post for user id 1
+  // const  createPost = await prisma.post.create({
+  //     data: {
+  //         title: "My first post",
+  //         content: "This is my first post",
+  //         authorId: 1,
+  //         // author: {
+  //         //     connect: {
+  //         //         id: 1
+  //         //     }
+  //         // }
+  //     }
+  // })
+
+  // console.log(createPost);
+
+  // created profile
+
+  const createdProfile = await prisma.profile.create({
+    data: {
+      bio: "I like turtles",
+      userId: 1,
+    },
+  });
+
+  console.log(createdProfile);
+}
+
+run();
